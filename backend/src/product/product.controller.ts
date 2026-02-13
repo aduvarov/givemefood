1677afff8834fdec9a657beae882b058bef22908
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-02-13 22:56:19 +0500

    Реализация ресурса - продукты

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,7 +1,56 @@
-import { Controller } from '@nestjs/common';
-import { ProductService } from './product.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	Query,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { ProductService } from './product.service'
+import { ProductDTO } from './dto/product.dto'
 
-@Controller('product')
+@Controller('products')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+	constructor(private readonly productService: ProductService) {}
+
+	@UsePipes(new ValidationPipe())
+	@Get()
+	async getAll(@Query('searchTerm') searchTerm?: string) {
+		return this.productService.getAll(searchTerm)
+	}
+
+	@Get('by-slug/:slug')
+	async getProductBySlug(@Param('slug') slug: string) {
+		return this.productService.bySlug(slug)
+	}
+
+	@Get('by-category/:categorySlug')
+	async getProductsByCategory(@Param('categorySlug') categorySlug: string) {
+		return this.productService.byCategory(categorySlug)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Post()
+	@HttpCode(200)
+	async create() {
+		return this.productService.create()
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() dto: ProductDTO) {
+		return this.productService.update(id, dto)
+	}
+
+	@HttpCode(200)
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		return this.productService.delete(id)
+	}
 }
